7c5e0c850909464b8c5af7735ce30583cc73a792
Programming Hero <programming.hero1@gmail.com>
2022-05-26 13:55:30 -0500

    basic types

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let student: string = 'Jeff';
+let age: number = 69;
+let isSmart: boolean = true;
+
+let students: string[] = ['Joe', 'Biden', 'Harris']; 
+let fees: number[] = [12,45,78,12,45];
+
+interface Person {
+  name: string,
+  job?: string, 
+  age: number
+}
+
+const person: Person = {
+  name: 'Bill Clinton',
+  age: 85
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
---
+++ src/vanila/vanila.js
@@ -0,0 +1,8 @@
+let user = 'Robert';
+user = 55;
+user = false;
+user = {id: 1, age: 25, address: 'K'}
+user = [1, 2,3,5, 5];
+user =['rob', 'john', 'smith'];
+user = () => 55;
+console.log(user);
